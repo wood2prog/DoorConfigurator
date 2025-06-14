ed30fb438ceda62df402293283d6bd72646e47bb
Jesse Ogburn <jogburn88@gmail.com>
2025-06-14 11:21:26 -0400

    Add method for creating the list of doors.

--- src/doors.py
+++ src/doors.py
@@ -1,13 +1,23 @@
-from src.import_data import ImportData
+from import_data import ImportData
 
 
 class Doors:
 
     def __init__(self):
+        self.header = []
         self.door_list = []
 
     def add_doors_from_file(self, file_name: str):
-        pass
+        door_list = ImportData(file_name)
+        self.header = door_list.get_header()
+        self.door_list = door_list.get_data()
 
     def clear_list(self):
         pass
+
+
+mydoors = Doors()
+
+mydoors.add_doors_from_file("tests/testdata.csv")
+print(mydoors.header)
+print(mydoors.door_list)
