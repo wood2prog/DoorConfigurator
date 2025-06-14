e79d538f745b7f725df1d2eee7d87fd16433cd07
Jesse Ogburn <jogburn88@gmail.com>
2025-06-14 10:40:01 -0400

    Add the doors class

---
+++ src/doors.py
@@ -0,0 +1,13 @@
+from src.import_data import ImportData
+
+
+class Doors:
+
+    def __init__(self):
+        self.door_list = []
+
+    def add_doors_from_file(self, file_name: str):
+        pass
+
+    def clear_list(self):
+        pass
